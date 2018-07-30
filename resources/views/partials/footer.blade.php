a27243a510e02813cb7b69bca7c0ba24e39b880c
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2018-07-30 11:47:41 -0400

    logo seremi

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,10 @@
 <footer class="content-info">
-  <div class="container">
-    @php(dynamic_sidebar('sidebar-footer'))
-  </div>
+	<div class="container-fluid">
+		@php(dynamic_sidebar('sidebar-footer'))
+		<div class="row no-gutters justify-content-end">
+			<div class="col-2 seremi">
+				<img src="{{ get_template_directory_uri() . '/assets/images/logo_seremi.png' }}" alt="SEREMI">
+			</div>
+		</div>
+	</div>
 </footer>
