5be4964d93cfa58bebdfa1326c7f16592ee7e4ea
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2018-07-30 12:18:31 -0400

    ajustes logo fondart

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,8 +2,10 @@
 	<div class="container-fluid">
 		@php(dynamic_sidebar('sidebar-footer'))
 		<div class="row no-gutters justify-content-end">
-			<div class="col-2 seremi">
-				<img src="{{ get_template_directory_uri() . '/assets/images/logo_seremi.png' }}" alt="SEREMI">
+			<div class="col-md-4 seremi">
+				<strong>Financia:</strong> <br>
+				<img src="{{ get_template_directory_uri() . '/assets/images/logo_seremi.png' }}" alt="SEREMI"><br>
+				Fondo Nacional de Desarrollo Cultural y las Artes, Fondart Regional
 			</div>
 		</div>
 	</div>
