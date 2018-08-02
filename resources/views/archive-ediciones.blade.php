dd39dd9a08c8ef2ba0d3900184db9b1225533102
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2018-08-02 12:10:42 -0400

    ajuste pag ediciones

--- resources/views/archive-ediciones.blade.php
+++ resources/views/archive-ediciones.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-  
 <div class="container">
   <h1 class="ediciones-title">Ediciones</h1>  
-  <div class="ediciones-content row">
-  @foreach($getediciones as $item)
-    @include('partials.content-edicioncard')
-  @endforeach
+  <div class="ediciones-content">
+    <div class="row">
+      @foreach($getediciones as $item)
+        @include('partials.content-edicioncard')
+      @endforeach
+    </div>
   </div>
-  </div>
+</div>
 @endsection
