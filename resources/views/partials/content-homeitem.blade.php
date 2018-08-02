bdc3443766dd7f7b0d9be56e0e0e2b6b42114eab
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2018-08-02 16:30:56 -0400

    cambios en reunion

--- resources/views/partials/content-homeitem.blade.php
+++ resources/views/partials/content-homeitem.blade.php
@@ -3,7 +3,6 @@
   	<a href="{{$item->link}}">
   		<h1>{{ $item->title }}</h1>
   		@include('partials/entry-meta-plain', ['author' => $item->author])
-  		<span class="post-type-name">{{ $item->type }}</span>
   	</a>
   	</div>
 </article>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,9 +3,11 @@
 		@php(dynamic_sidebar('sidebar-footer'))
 		<div class="row no-gutters justify-content-end">
 			<div class="col-md-4 seremi">
+				@if(is_home())
 				<strong>Financia:</strong> <br>
 				<img src="{{ get_template_directory_uri() . '/assets/images/logo_seremi.png' }}" alt="SEREMI"><br>
 				Fondo Nacional de Desarrollo Cultural y las Artes, Fondart Regional
+				@endif
 			</div>
 		</div>
 	</div>
