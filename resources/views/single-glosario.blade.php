605ab28efce1487759ff7cb7f05b8ba02f4d0c6e
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-07-27 17:57:49 -0400

    ajuste single blade

--- resources/views/single-glosario.blade.php
+++ resources/views/single-glosario.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
+  @include('partials.page-header')
   @while(have_posts()) @php(the_post())
-    @include('partials.page-header')
     <div class="entry-content">
     	@include('partials.content-page')
     </div>
