608ee9b79656bc158203ad91aacca694f19b640b
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-07-27 17:59:10 -0400

    single blade glosario

--- resources/views/single-glosario.blade.php
+++ resources/views/single-glosario.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-  @include('partials.page-header')
   @while(have_posts()) @php(the_post())
     <div class="entry-content">
     	@include('partials.content-page')
