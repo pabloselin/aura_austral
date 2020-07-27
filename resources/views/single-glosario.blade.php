bad4aa5b83668eab751424c863e39d50947a41db
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-07-27 18:02:43 -0400

    ajuste glosario

--- resources/views/single-glosario.blade.php
+++ resources/views/single-glosario.blade.php
@@ -3,7 +3,11 @@
 <div class="container">
   @while(have_posts()) @php(the_post())
     <div class="entry-content">
-    	@include('partials.content-page')
+    	<div class="page-header">
+    		<h1 class="entry-title"> @php(get_the_title()) </h1>
+    	</div>
+
+    	 @include('partials.content-page')
     </div>
   @endwhile
   </div>
